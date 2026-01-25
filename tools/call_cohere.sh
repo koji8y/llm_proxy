4b3d01af6668d944191c167139e6c0904bb5c9ad
Koji Yamamoto <kouji@acm.org>
2026-01-26 07:08:43 +0900

    refactor: introduce templates
    Signed-off-by: Koji Yamamoto <kouji@acm.org>

--- tools/call_cohere.sh
+++ tools/call_cohere.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
+model=command-a-03-2025
+version=1
+stream=false
+message="Who is the prime minister of Japan?"
+
+here="$(cd "$(dirname $0)"; pwd -P)"
+if [ -f .env ]; then
+    source .env
+fi
 help_exit() {
-  echo "Usage: $(basename $0) [-s|-n] [-1|-2] [-H path_to_header] "
+  echo "Usage: $(basename $0) [-s|-n] [-1|-2] [-H path_to_header] [-m message]"
   echo ''
   echo "  -s: stream"
   echo "  -n: non-stream (default)"
@@ -8,17 +17,39 @@ help_exit() {
   echo "  -2: version 2"
   exit 1
 }
-version=1
-stream=ns
-header=tmp/header_cohere
+header_path="/tmp/header.$$"
+body_path="/tmp/body.$$"
+clean_up() {
+  rm -f "${header_path}" "${body_path}"
+}
+trap clean_up 0 2 9 15
 while getopts "sn12H:h" opt; do case "${opt}" in
-  s) stream=s ;;
-  n) stream=ns ;;
+  s) stream=true ;;
+  n) stream=false ;;
   1) version=1 ;;
   2) version=2 ;;
   H) header="${OPTARG}" ;;
+  m) message="${OPTARG}" ;;
   h|\?) help_exit ;;
 esac; done
 
+header_template="${here}/header_cohere_template"
+body_template="${here}/body_coherev${version}_template"
+
+if [ -z "${header}" ]; then
+  cat "${header_template}" | sed -e 's/${CO_API_KEY}/'"${CO_API_KEY}"'/' > "${header_path}"
+else
+  echo "${header}" > "${heeader_path}"
+fi
+echo "Header:"
+cat "${header_path}"
+
+cat "${body_template}" \
+  | sed -e 's/${model}/'"${model}"'/;s/${message}/'"${message}"'/;s/${stream}/'"${stream}"'/' \
+  > "${body_path}"
+echo "Body:"
+cat "${body_path}"
+
 set -ex
-curlh -X POST --data @tmp/bodyv${version}${stream} -H @${header} --url http://127.0.0.1:8000/v${version}/chat
+#curlh -X POST --data @tmp/bodyv${version}${stream} -H @${header_path} --url http://127.0.0.1:8000/v${version}/chat
+curlh -X POST --data @${body_path} -H @${header_path} --url http://127.0.0.1:8000/v${version}/chat
