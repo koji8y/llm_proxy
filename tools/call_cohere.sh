6035ae5b58cb4e176fe04591137d36dbf0fc5c73
Koji Yamamoto <kouji@acm.org>
2026-01-26 18:29:20 +0900

    feat: support openai API; support listing available models
    Signed-off-by: Koji Yamamoto <kouji@acm.org>

--- tools/call_cohere.sh
+++ tools/call_cohere.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-model=command-a-03-2025
-version=1
 stream=false
 message="Who is the prime minister of Japan?"
 
@@ -8,13 +6,20 @@ here="$(cd "$(dirname $0)"; pwd -P)"
 if [ -f .env ]; then
     source .env
 fi
+
+curlh() {
+  echo "curl $@" 1>&2
+  curl -N --no-progress-meter -D /dev/stderr "$@"
+}
 help_exit() {
-  echo "Usage: $(basename $0) [-s|-n] [-1|-2] [-H path_to_header] [-m message]"
+  echo "Usage: $(basename $0) [-s|-n] [-1|-2|-o] [-H path_to_header] [-m message] [-u base_url] [-k key_variable] [-M]"
   echo ''
   echo "  -s: stream"
   echo "  -n: non-stream (default)"
-  echo "  -1: version 1 (default)"
-  echo "  -2: version 2"
+  echo "  -1: Cohere version 1 (default)"
+  echo "  -2: Cohereversion 2"
+  echo "  -o: OpenAI"
+  echo "  -M: get models instead of chatting"
   exit 1
 }
 header_path="/tmp/header.$$"
@@ -23,33 +28,68 @@ clean_up() {
   rm -f "${header_path}" "${body_path}"
 }
 trap clean_up 0 2 9 15
-while getopts "sn12H:h" opt; do case "${opt}" in
+while getopts "sn12oH:u:k:Mh" opt; do case "${opt}" in
   s) stream=true ;;
   n) stream=false ;;
-  1) version=1 ;;
-  2) version=2 ;;
+  1) cohere_version=1 ;;
+  2) cohere_version=2 ;;
+  o) openai=yes ;;
   H) header="${OPTARG}" ;;
   m) message="${OPTARG}" ;;
+  u) base_url="${OPTARG}" ;;
+  k) key_variable="${OPTARG}" ;;
+  M) get_models=yes ;;
   h|\?) help_exit ;;
 esac; done
 
-header_template="${here}/header_cohere_template"
-body_template="${here}/body_coherev${version}_template"
+if [ -n "${cohere_version}" ]; then
+  header_template="${here}/header_cohere_template"
+  body_template="${here}/body_coherev${cohere_version}_template"
+  path="v${cohere_version}/chat"
+  model="${model:- command-a-03-2025}"
+  base_url="${base_url:-https://api.cohere.com/}"
+  key_variable="${key_variable:-CO_API_KEY}"
+elif [ -n "${openai}" ]; then
+  header_template="${here}/header_openai_template"
+  body_template="${here}/body_openai_template"
+  path="v1/chat/completions"
+  model="${model:- gpt-5}"
+  base_url="${base_url:-https://api.openai.com/}"
+  key_variable="${key_variable:-OPENAI_API_KEY}"
+else
+  echo "Error: Specify either -1 or -2 for Cohere version, or -o for OpenAI." >&2
+  help_exit
+fi
+base_url="${base_url%/}"
+
 
 if [ -z "${header}" ]; then
-  cat "${header_template}" | sed -e 's/${CO_API_KEY}/'"${CO_API_KEY}"'/' > "${header_path}"
+  cat "${header_template}" \
+    | sed -e 's/${API_KEY}/'"${!key_variable}"'/' \
+    > "${header_path}"
 else
   echo "${header}" > "${heeader_path}"
 fi
-echo "Header:"
-cat "${header_path}"
+echo "[Header:]" 1>&2
+cat "${header_path}" 1>&2
+
+if [ -n "${get_models}" ]; then
+  path="${path%/}"
+  path="${path%/completions}"
+  path="${path%/chat}/models"
+  # cat "${body_template}" \
+  #   | sed -e 's/${model}/'"${model}"'/;s/${message}/'"${message}"'/;s/${stream}/'"${stream}"'/' \
+  #   > "${body_path}"
+  # echo "[Body:]"
+  # cat "${body_path}"
+  curlh -X GET -H @${header_path} --url ${base_url}/${path}
+  exit 0
+fi
 
 cat "${body_template}" \
   | sed -e 's/${model}/'"${model}"'/;s/${message}/'"${message}"'/;s/${stream}/'"${stream}"'/' \
   > "${body_path}"
-echo "Body:"
-cat "${body_path}"
+echo "[Body:]" 1>&2
+cat "${body_path}" 1>&2
 
-set -ex
-#curlh -X POST --data @tmp/bodyv${version}${stream} -H @${header_path} --url http://127.0.0.1:8000/v${version}/chat
-curlh -X POST --data @${body_path} -H @${header_path} --url http://127.0.0.1:8000/v${version}/chat
+curlh -X POST --data @${body_path} -H @${header_path} --url ${base_url}/${path}
