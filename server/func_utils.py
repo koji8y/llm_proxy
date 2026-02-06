ca8f2605e984fb15c8f16c4404a4e798308f18b4
Koji Yamamoto <kouji@acm.org>
2026-02-06 13:08:37 +0000

    fix: support the case that a certain type of exceptions is a secondary normal case
    Signed-off-by: Koji Yamamoto <kouji@acm.org>

--- server/func_utils.py
+++ server/func_utils.py
@@ -1,20 +1,35 @@
 from typing import Callable
 import inspect
 
-def show_result(func, to_show: Callable[[], bool] | None = None, **info):
+def show_result(func, to_show: Callable[[], bool] | None = None, exception_types_to_omit_traceback: tuple[type[Exception], ...] | None = None, **info):
+    def default_func_to_show() -> bool:
+        return False
+
+    if to_show is None:
+        to_show = default_func_to_show
+
     async def wrapper_async(*args, **kwargs):
         if to_show is not None and to_show():
             from icecream import ic
         try:
-            result = '(unassigned due to exception)'
+            # result = '(unassigned due to exception)'
             result = await func(*args, **kwargs)
-        except Exception:
-            if to_show is not None and to_show():
+        except Exception as exp:
+            # if to_show is not None and to_show():
+            #     if info:
+            #         ic('raising exception', func.__name__, info, type(result), result)
+            #     else:
+            #         ic('raising exception', func.__name__, type(result), result)
+            #     raise
+            # from icecream import ic; ic(func.__name__, 'caught exception', exp)
+            # from traceback import print_exc; print_exc()
+            if to_show() and (exception_types_to_omit_traceback is None or not isinstance(exp, exception_types_to_omit_traceback)):
                 if info:
                     ic('raising exception', func.__name__, info, type(result), result)
                 else:
                     ic('raising exception', func.__name__, type(result), result)
-                raise
+                from traceback import print_exc; print_exc()
+            
         if to_show is not None and to_show():
             if info:
                 ic(func.__name__, info, type(result), result)
@@ -26,15 +41,23 @@ def show_result(func, to_show: Callable[[], bool] | None = None, **info):
         if to_show is not None and to_show():
             from icecream import ic
         try:
-            result = '(unassigned due to exception)'
+            # result = '(unassigned due to exception)'
             result = func(*args, **kwargs)
-        except Exception:
-            if to_show is not None and to_show():
+        except Exception as exp:
+            # if to_show is not None and to_show():
+            #     if info:
+            #         ic('raising exception', func.__name__, info, type(result), result)
+            #     else:
+            #         ic('raising exception', func.__name__, type(result), result)
+            #     raise
+            # from icecream import ic; ic(func.__name__, 'caught exception', exp)
+            # from traceback import print_exc; print_exc()
+            if to_show() and (exception_types_to_omit_traceback is None or not isinstance(exp, exception_types_to_omit_traceback)):
                 if info:
                     ic('raising exception', func.__name__, info, type(result), result)
                 else:
                     ic('raising exception', func.__name__, type(result), result)
-                raise
+                from traceback import print_exc; print_exc()
         if to_show is not None and to_show():
             if info:
                 ic(func.__name__, info, type(result), result)
@@ -50,12 +73,12 @@ def show_result(func, to_show: Callable[[], bool] | None = None, **info):
     return wrapper
 
 
-def show_result_with_control(enable: bool = True, to_show: Callable[[], bool] | None = None, **info):
+def show_result_with_control(enable: bool = True, to_show: Callable[[], bool] | None = None, exception_types_to_omit_traceback: tuple[type[Exception], ...] | None = None, **info):
     if not enable:
         def decorator(func):
             return func
         return decorator
 
     def decorator(func):
-        return show_result(func, to_show=to_show, **info)
+        return show_result(func, to_show=to_show, exception_types_to_omit_traceback=exception_types_to_omit_traceback, **info)
     return decorator
